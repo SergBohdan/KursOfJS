626bdb3af8763963a10773f56ff8ba235cb668f5
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-07-29 15:37:05 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -820,3 +820,67 @@
 //   // Change code above this line
 // }
 // addOverNum(10, 12, 4, 11, 48, 10, 8);
+
+
+// function registerGuest(name, callback) {
+//     console.log(`Реєструємо гостя ${name}.`);
+//     callback(name);
+//   }
+  
+//   // Передаємо інлайн функцію greet у якості колбека
+//   registerGuest("Манго", function greet(name) {
+//     console.log(`Ласкаво просимо ${name}.`);
+//   });
+  
+//   // Передаємо інлайн функцію notify у якості колбека
+//   registerGuest("Полі", function notify(name) {
+//     console.log(`Шановний(а) ${name}, ваш номер буде готовий за 30 хвилин.`);
+//   });
+
+// function processCall(recipient) {
+//   // Імітуємо доступність абонента випадковим числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+//   console.log('Bolean', isRecipientAvailable);
+//   console.log(Math.random());
+
+//   if (!isRecipientAvailable) {
+//     console.log(`Абонент ${recipient} недоступний, залиште повідомлення.`);
+//     // Логіка активації автовідповідача
+//   } else {
+//     console.log(`З'єднуємо з ${recipient}, очікуйте...`);
+//     // Логіка прийняття дзвінка
+//   }
+// }
+
+// processCall("Манго");
+
+// function processCall(recipient, onAvailable, onNotAvailable) {
+//   // Імітуємо доступність абонента випадковим числом
+//   const isRecipientAvailable = Math.random() > 0.5;
+
+//   if (!isRecipientAvailable) {
+//     onNotAvailable(recipient);
+//     return;
+//   }
+
+//   onAvailable(recipient);
+// }
+
+// function takeCall(name) {
+//   console.log(`З'єднуємо з ${name}, очікуйте...`);
+//   // Логіка прийняття дзвінка
+// }
+
+// function activateAnsweringMachine(name) {
+//   console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
+//   // Логіка активації автовідповідача
+// }
+
+// function leaveHoloMessage(name) {
+//   console.log(`Абонент ${name} недоступний, записуємо голограму.`);
+//   // Логіка запису голограми
+// }
+
+// processCall("Манго", takeCall, activateAnsweringMachine);
+// processCall("Полі", takeCall, leaveHoloMessage);
+
