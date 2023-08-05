1453afc44bd09d891d5c523e38e28731037a964b
Serhii Bogdanov <bohdanov.serhey@gmail.com>
2023-08-05 10:17:30 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -884,3 +884,83 @@
 // processCall("Манго", takeCall, activateAnsweringMachine);
 // processCall("Полі", takeCall, leaveHoloMessage);
 
+// import axios from 'axios';
+// import {
+//    getAllCategories,
+//    getTopBooks,
+//    getSeparateCategories,
+//    getBookInfo
+// } from './api.js';
+
+
+
+
+// const categoriesAll = "https://books-backend.p.goit.global/books/category-list";
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const libraryLinks = document.querySelectorAll('.library-link');
+//   const existingCategoryLinks = document.querySelectorAll('.category-link');
+
+//   libraryLinks.forEach((link) => {
+//     link.addEventListener('click', async (event) => {
+//       event.preventDefault();
+//       const selectedCategory = link.dataset.category;
+//       try {
+//         const response = await axios.get(`https://books-backend.p.goit.global/books/category?category=${selectedCategory}`);
+//         renderContent(selectedCategory, response.data);
+//       } catch (error) {
+//         console.error('Error fetching books:', error);
+//         renderError();
+//       }
+//     });
+//   });
+
+//   existingCategoryLinks.forEach((link) => {
+//     link.addEventListener('click', async (event) => {
+//       event.preventDefault();
+//       const selectedCategory = link.dataset.category;
+//       try {
+//         const response = await axios.get(`https://books-backend.p.goit.global/books/category?category=${selectedCategory}`);
+//         renderContent(selectedCategory, response.data);
+//       } catch (error) {
+//         console.error('Error fetching books:', error);
+//         renderError();
+//       }
+//     });
+//   });
+// });
+
+// function renderContent(category, books) {
+//   const contentContainer = document.querySelector('.content-container');
+
+//   let booksMarkup = '';
+//   if (books.length === 0) {
+//     booksMarkup = '<p>No books found in this category.</p>';
+//   } else {
+//     books.forEach(book => {
+//       booksMarkup += createMarkup(book);
+//     });
+//   }
+
+//   const categoryMarkup = `<h2>${category}</h2>`;
+//   contentContainer.innerHTML = categoryMarkup + booksMarkup;
+// }
+
+// function createMarkup(book) {
+//   return `
+//     <div class="book">
+//       <img src="${book.book_image}" alt="${book.title}" />
+//       <p>${book.title} by ${book.author}</p>
+//     </div>
+//   `;
+// }
+
+// function renderError() {
+//   const contentContainer = document.querySelector('.content-container');
+//   contentContainer.innerHTML = '<p>An error occurred while fetching books.</p>';
+// }
+
+// getAllCategories();
+// getTopBooks();
+// getSeparateCategories('Combined Print and E-book Fiction');
+// getBookInfo('some_book_id');
